10799996c7cc9d298989ae3052917f5844ebbee3
Guilherme Gois <guijocargo@gmail.com>
2020-04-22 20:49:14 -0300

    fix(js): add missing declarations file for Colors

---
+++ js/Colors.d.ts
@@ -0,0 +1,36 @@
+// *******************************************************
+// Generated with:
+//  https://www.typescriptlang.org/play/?useJavaScript=true
+// *******************************************************
+declare namespace _default {
+    export const Transparent: string;
+    export const White: string;
+    export const Black: string;
+    export const ExtraDarkGray: string;
+    export const DarkGray: string;
+    export const MediumGray: string;
+    export const VeryLightGray: string;
+    export const BaseBackground: string;
+    export const Lightbox: string;
+    export const GorilaPrimary: string;
+    export const GorilaSecondary: string;
+    export const FixedIncomePrimary: string;
+    export const FixedIncomeSecondary: string;
+    export const StocksPrimary: string;
+    export const StocksSecondary: string;
+    export const MultimarketPrimary: string;
+    export const MultimarketSecondary: string;
+    export const CurrencyPrimary: string;
+    export const CurrencySecondary: string;
+    export const TangiblePrimary: string;
+    export const TangibleSecondary: string;
+    export const CashPrimary: string;
+    export const CashSecondary: string;
+    export const Positive: string;
+    export const Negative: string;
+    export const BuyPrimary: string;
+    export const BuySecondary: string;
+    export const SellPrimary: string;
+    export const SellSecondary: string;
+}
+export default _default;
